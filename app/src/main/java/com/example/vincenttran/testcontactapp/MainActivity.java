af16b7a6dc3cd4648871c6202e26efdb9a9be6be
VincentTr4n <33866720+VincentTr4n@users.noreply.github.com>
2019-04-27 15:25:11 +0700

    update keyboard

--- app/src/main/java/com/example/vincenttran/testcontactapp/MainActivity.java
+++ app/src/main/java/com/example/vincenttran/testcontactapp/MainActivity.java
@@ -10,11 +10,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
+import android.view.HapticFeedbackConstants;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
     private ExampleAdapter adapter;
     private List<ExampleItem> exampleList;
+
+    private TextView btnNumber1, btnNumber2, btnNumber3, btnNumber4, btnNumber5, btnNumber6,
+            btnNumber7, btnNumber8, btnNumber9, btnAsterisk, btnHashtag;
+    private ImageView btnClear;
+    private RelativeLayout btnNumber0;
+    private EditText txtInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
         fillExampleList();
         setUpRecyclerView();
+        initView();
     }
 
     public static void requestPermission(Activity activity, String permission) {
@@ -50,6 +66,8 @@ public class MainActivity extends AppCompatActivity {
         adapter = new ExampleAdapter(exampleList);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
+
+        adapter.getFilter().filter("");
 
         adapter.setClickListener(new ExampleAdapter.ClickListener() {
             @Override
@@ -77,6 +95,86 @@ public class MainActivity extends AppCompatActivity {
 //        exampleList.add(new ExampleItem("Nine", "Eighteen"));
         ContactUtils utils = new ContactUtils(this);
         exampleList = utils.getAllContacts();
+    }
+
+    private void initView(){
+        btnNumber0  = findViewById(R.id.dialpad_0_holder);
+        btnNumber1  = findViewById(R.id.dialpad_1);
+        btnNumber2  = findViewById(R.id.dialpad_2);
+        btnNumber3  = findViewById(R.id.dialpad_3);
+        btnNumber4  = findViewById(R.id.dialpad_4);
+        btnNumber5  = findViewById(R.id.dialpad_5);
+        btnNumber6  = findViewById(R.id.dialpad_6);
+        btnNumber7  = findViewById(R.id.dialpad_7);
+        btnNumber8  = findViewById(R.id.dialpad_8);
+        btnNumber9  = findViewById(R.id.dialpad_9);
+        btnAsterisk = findViewById(R.id.dialpad_asterisk);
+        btnHashtag  = findViewById(R.id.dialpad_hashtag);
+        btnClear    = findViewById(R.id.dialpad_clear_char);
+        txtInput    = findViewById(R.id.dialpad_input);
+
+        txtInput.setShowSoftInputOnFocus(false);
+
+        btnNumber0.setOnClickListener(v -> { keyPress('0', v); });
+        btnNumber1.setOnClickListener(v -> { keyPress('1', v); });
+        btnNumber2.setOnClickListener(v -> { keyPress('2', v); });
+        btnNumber3.setOnClickListener(v -> { keyPress('3', v); });
+        btnNumber4.setOnClickListener(v -> { keyPress('4', v); });
+        btnNumber5.setOnClickListener(v -> { keyPress('5', v); });
+        btnNumber6.setOnClickListener(v -> { keyPress('6', v); });
+        btnNumber7.setOnClickListener(v -> { keyPress('7', v); });
+        btnNumber8.setOnClickListener(v -> { keyPress('8', v); });
+        btnNumber9.setOnClickListener(v -> { keyPress('9', v); });
+        btnAsterisk.setOnClickListener(v -> { keyPress('*', v); });
+        btnHashtag.setOnClickListener(v -> { keyPress('#', v); });
+
+        btnClear.setOnClickListener(v -> {
+            txtInput.dispatchKeyEvent(new KeyEvent(0, 0, KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL, 0));
+        });
+
+        btnClear.setOnLongClickListener(v -> {
+            txtInput.setText("");
+            return true;
+        });
+
+        btnNumber0.setOnLongClickListener(v -> {
+            keyPress('+', v);
+            return true;
+        });
+
+        txtInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                adapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) { }
+        });
+    }
+
+    private void keyPress(char c, View view) {
+        txtInput.dispatchKeyEvent(new KeyEvent(0, 0, KeyEvent.ACTION_DOWN, getCharKeyCode(c), 0));
+        view.performHapticFeedback(HapticFeedbackConstants.KEYBOARD_PRESS);
+    }
+
+    private char getCharKeyCode(char c){
+        if(c == '0') return KeyEvent.KEYCODE_0;
+        if(c == '1') return KeyEvent.KEYCODE_1;
+        if(c == '2') return KeyEvent.KEYCODE_2;
+        if(c == '3') return KeyEvent.KEYCODE_3;
+        if(c == '4') return KeyEvent.KEYCODE_4;
+        if(c == '5') return KeyEvent.KEYCODE_5;
+        if(c == '6') return KeyEvent.KEYCODE_6;
+        if(c == '7') return KeyEvent.KEYCODE_7;
+        if(c == '8') return KeyEvent.KEYCODE_8;
+        if(c == '9') return KeyEvent.KEYCODE_9;
+        if(c == '*') return KeyEvent.KEYCODE_STAR;
+        if(c == '+') return KeyEvent.KEYCODE_PLUS;
+        return KeyEvent.KEYCODE_POUND;
     }
 
     @Override
